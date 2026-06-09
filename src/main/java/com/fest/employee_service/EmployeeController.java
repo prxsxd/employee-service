7b2fe2a10837896bcbc077a2f2c37a942da9a285
Prasad Bhogekar <prasadbhogekar@gmail.com>
2026-06-09 23:25:56 +0530

    Initial Spring Boot application

---
+++ src/main/java/com/fest/employee_service/EmployeeController.java
@@ -0,0 +1,15 @@
+package com.fest.employee_service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello from Employee Service!";
+    }
+}
